beb46910f4bd5041dcf174217a8199855c97db65
shino <jaredmc32@gmail.com>
2023-09-16 22:29:39 -0500

    declaring these structs is taking foreverrr

--- src/util/chess.rs
+++ src/util/chess.rs
@@ -1,6 +1,6 @@
 
 use std::{str::*, slice::Chunks};
-
+use std::ops::{Index, IndexMut};
 
 
 use num_enum::IntoPrimitive;
@@ -54,8 +54,77 @@ const default_pieces:[u8;32] = [
 
 
 
-
-
+pub struct PieceSet {
+  w_king:Vec<u8>,
+  b_king:Vec<u8>,
+  w_queen:Vec<u8>,
+  b_queen:Vec<u8>,
+  w_rook:Vec<u8>,
+  b_rook:Vec<u8>,
+  w_bishop:Vec<u8>,
+  b_bishop:Vec<u8>,
+  w_knight:Vec<u8>,
+  b_knight:Vec<u8>,
+  w_pawn:Vec<u8>,
+  b_pawn:Vec<u8>
+}
+impl Default for PieceSet{
+  fn default() -> Self {
+      return PieceSet {
+        w_king: vec![60],
+        b_king: vec![4],
+        w_queen: vec![59],
+        b_queen: vec![3],
+        w_rook: vec![63, 56],
+        b_rook: vec![0, 7],
+        w_bishop: vec![61, 58],
+        b_bishop: vec![2, 5],
+        w_knight: vec![62, 57],
+        b_knight: vec![1, 6],
+        w_pawn: vec![47, 48, 49, 50, 51, 52, 53, 54,],
+        b_pawn: vec![8, 9, 10, 11, 12, 13, 14, 15]
+      }
+  }
+}
+impl Index<Piece> for PieceSet {
+  type Output = Vec<u8>;
+  fn index(&self, index: Piece) -> &Self::Output {
+      match index {
+          Piece::WKing => &self.w_king,
+          Piece::WQueen => &self.w_queen,
+          Piece::WRook => &self.w_rook,
+          Piece::WBishop => &self.w_bishop,
+          Piece::WKnight => &self. w_knight,
+          Piece::WPawn => &self.w_pawn,
+          Piece::BKing => &self.b_king,
+          Piece::BQueen => &self.b_queen,
+          Piece::BRook => &self.b_rook,
+          Piece::BBishop => &self.b_bishop,
+          Piece::BKnight => &self.b_knight,
+          Piece::BPawn => &self.b_pawn,
+          Piece::Empty => &vec![65]
+      }
+  }
+}
+impl IndexMut<Piece> for PieceSet {
+  fn index_mut(&mut self, index: Piece) -> &mut Self::Output {
+      match index {
+          Piece::WKing => &mut self.w_king,
+          Piece::WQueen => &mut self.w_queen,
+          Piece::WRook => &mut self.w_rook,
+          Piece::WBishop => &mut self.w_bishop,
+          Piece::WKnight => &mut self. w_knight,
+          Piece::WPawn => &mut self.w_pawn,
+          Piece::BKing => &mut self.b_king,
+          Piece::BQueen => &mut self.b_queen,
+          Piece::BRook => &mut self.b_rook,
+          Piece::BBishop => &mut self.b_bishop,
+          Piece::BKnight => &mut self.b_knight,
+          Piece::BPawn => &mut self.b_pawn,
+          Piece::Empty => &mut vec![65]
+      }
+  }
+}
 
 pub struct Move {
   from: usize,
@@ -64,7 +133,7 @@ pub struct Move {
 
 pub struct Board {
   sq: [u8; 64],
-  pieces: [u8, 32],
+  pieces: [u8; 32],
   castle_state: [bool; 4],
   en_pessant_sq: u8,
   turn: u8, // 0: white, 1: black
@@ -72,7 +141,7 @@ pub struct Board {
 }
 impl Default for Board {
   fn default() -> Self {
-    Board {sq:[0; 64], pieces:[0, 32], castle_state: [true; 4], en_pessant_sq: 64, turn: 0, draw_count: 0}
+    Board {sq:[0; 64], pieces:[0; 32], castle_state: [true; 4], en_pessant_sq: 64, turn: 0, draw_count: 0}
   }
 }
 
